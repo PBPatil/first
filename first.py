61d5ec434b50a464c27e2d7c8399d80822f74d42
Prasad Patil <theprasadpatil@gmail.com>
2018-01-13 16:35:55 +0530

    remo hell world

--- first.py
+++ first.py
@@ -1,13 +1,5 @@
-
 print'Good Morning'
 print 'good afternoon'
 print 'Good Night'
-print 'Hello world'
 print 'Hie'
-
-
-print'Good Morning'
-print 'Good Afternoon'
-print 'Good night'
-print 'Great'
 
